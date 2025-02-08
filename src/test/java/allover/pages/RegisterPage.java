5ca9b3df5eb851666ef1e473d31a50a95028afe9
Hakan Kanpolat <hakan.kanpolat04@gmail.com>
2025-02-08 20:58:15 +0100

    RegisterPageDone

---
+++ src/test/java/allover/pages/RegisterPage.java
@@ -0,0 +1,45 @@
+package allover.pages;
+
+import allover.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RegisterPage {
+
+    public RegisterPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//input[@id='reg_username']")
+    public WebElement UsernameTextBox;
+
+    @FindBy(xpath = "//input[@type='email']")
+    public WebElement MailAddressTextBox;
+
+    @FindBy(xpath = "//input[@id='reg_password']")
+    public WebElement PasswordTextBox;
+
+    @FindBy(xpath = "(//input[@id='register-policy'])[2]")
+    public WebElement AgreeCheckBox;
+
+    @FindBy(xpath = "(//button[@value='Sign Up'])[1]")
+    public WebElement SignUpButton;
+
+    @FindBy(partialLinkText = "Signup as a vendor?")
+    public WebElement VendorSignUpButton;
+
+    @FindBy(partialLinkText = "Become a Vendor")
+    public WebElement BecomeVendorButton;
+
+
+
+
+
+
+
+
+
+
+
+}
