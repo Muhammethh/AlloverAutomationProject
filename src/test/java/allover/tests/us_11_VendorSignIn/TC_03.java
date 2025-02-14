37d43b953004ee9184ad4253ab6dead9cb9dad23
Emre Pshigusa <emrepshigusa@gmail.com>
2025-02-14 16:52:26 +0100

    US_11 VendorSignIn test completed

--- src/test/java/allover/tests/us_11_VendorSignIn/TC_03.java
+++ src/test/java/allover/tests/us_11_VendorSignIn/TC_03.java
@@ -1,4 +1,4 @@
-package allover.tests.US_11;
+package allover.tests.us_11_VendorSignIn;
 
 import allover.pages.HomePage;
 import allover.pages.MyAccountPage;
@@ -6,52 +6,47 @@ import allover.pages.SignInPage;
 import allover.utilities.ConfigReader;
 import allover.utilities.Driver;
 import allover.utilities.ExtentReportsListener;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
+public class TC_03 {
 
-public class InvalidEmailTest {
-
-    @Test (description = "US-11 TC-02 Email alanı eksik bırakıldığında sayfaya giris yapilamamali (Negative Case)")
-
-    public void invalidEmailTest() {
+    @Test (description = "US-11 TC-03 Gecersiz password ile Sign in yapılamamali (Negative case)")
+    public void invalidPasswordTest() {
 
         //Vendor sign-in sayfasina gilidir
         Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
         ExtentReportsListener.extentTestInfo("allovercommerce anasayfasnia gidilir");
 
         //Sign-in linkine tıklanır
+
         HomePage homePage = new HomePage();
         homePage.signIn.click();
         ExtentReportsListener.extentTestInfo("Sign-in butonuna tiklanir");
 
-        //Email kısmına geçersiz veri girilir
+        //Email kısmına geçerli veri girilir
         SignInPage signInPage = new SignInPage();
-        signInPage.UsernameTextBox.sendKeys(ConfigReader.getProperty("invalidVendorEmail"));
+        signInPage.UsernameTextBox.sendKeys(ConfigReader.getProperty("vendorEmail"));
+        ExtentReportsListener.extentTestInfo("Gecerli email adresi girilir");
 
 
-        ExtentReportsListener.extentTestInfo("Gecersiz vendor email girilir");
-
-        //Password kısmına geçerli veri girilir
-        signInPage.PasswordTextBox.sendKeys(ConfigReader.getProperty("vendorPassword"));
-        ExtentReportsListener.extentTestInfo("Gecerli password girilir");
+        //Password kısmına geçersiz veri girilir
+        signInPage.PasswordTextBox.sendKeys(ConfigReader.getProperty("invalidPassword"));
+        ExtentReportsListener.extentTestInfo("Gecersiz vendor password girilir");
 
         //Sign-in butonuna tıklanır
         signInPage.SignInButton.click();
-
         ExtentReportsListener.extentTestInfo("Sign-in butonuna tiklanir");
 
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(5));
 
         //Sign-in olunamadığı doğrulanır
+
         MyAccountPage myAccountPage = new MyAccountPage();
         Assert.assertTrue(myAccountPage.WrongUsernamePasswordWarning.isDisplayed());
-        ExtentReportsListener.extentTestInfo("Gecersiz email ile giris yapilamadigi dogrulanir");
+        ExtentReportsListener.extentTestInfo("Gecersiz password ile giris yapilamadigi dogrulanir");
 
-     Driver.closeDriver();
+        Driver.closeDriver();
 
 
     }
