6a38b26b2ed16d7ea7834d4d7ad88d65a3fe0ffe
Hakan Kanpolat <hakan.kanpolat04@gmail.com>
2025-02-15 17:04:37 +0100

    RegisterNegative

--- src/test/java/allover/tests/US_01_Register/TC01_Register.java
+++ src/test/java/allover/tests/US_01_Register/TC01_Register.java
@@ -5,82 +5,61 @@ import allover.pages.RegisterPage;
 import allover.utilities.ConfigReader;
 import allover.utilities.Driver;
 import allover.utilities.ExtentReportsListener;
+import allover.utilities.ReusableMethods;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 
 public class TC01_Register {
 
-    @Test(description = "Siteye daha önce kayıtlı bilgiler ile(username ve email) kayıt yapılamamalı.(Register)")
-    public void testName() {
 
-        HomePage homePage= new HomePage();
-        RegisterPage registerPage= new RegisterPage();
-        ExtentReportsListener extentReportsListener=new ExtentReportsListener();
+    @DataProvider
+    public Object[][] emails() {
+        return new Object[][]{
+                {"Hakan1","Hakan1.techproed@gmail.com","malaky.charith123"},    // True Geçerli Gmail adresi
+                {"","Hakan2.techproed@gmail.com","malaky.charith123"},   // False Username alani bos birakildiginda
+                {"Hakan2","", "malaky.charith123"},    // False Your email address alani bos birakildiginda
+                {"Hakan3","Hakan3.techproed@gmail.com", ""},    // False Password alani bos birakildiginda
+                {"Hakan4","Hakan4.techproedgmail.com", "malaky.charith123"},   // False @ uzantisi yazilmadiginda
+                {"Hakan5","Hakan5.techproed", "malaky.charith123"},   // False @gmail.com uzantisi yazilmadiginda
 
-        ExtentReportsListener.extentTestInfo("sayfaya gidilir");
+                // Password alanina 8 karakterden az bir data girilmesine ragmen kayit islemi gerceklesmistir.
+
+                {"Hakan6","Hakan6.techproed@gmail.com","mal.123"}    // False Password alanina 8 karakterden az data yazildiginda
+        };
+    }
+
+    @Test(dataProvider = "emails")
+    public void test01(String username, String email, String password) {
+
+        // allover Sayfasına Gidelim
         Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
 
-        ExtentReportsListener.extentTestInfo("register butonuna tıklanır");
+        // Register butonuna tıklayalım
+        HomePage homePage = new HomePage();
         homePage.register.click();
 
-        ExtentReportsListener.extentTestInfo("Username bölümüne önceden kayıt olunan bir username girilir");
-        registerPage.UsernameTextBox.sendKeys(ConfigReader.getProperty("registerPositiveUsername"));
+        RegisterPage registerPage = new RegisterPage();
+        registerPage.UsernameTextBox.sendKeys(username);
 
-        ExtentReportsListener.extentTestInfo("Email bölümüne kayıtlı bir email girilir");
-        registerPage.MailAddressTextBox.sendKeys(ConfigReader.getProperty("registerPositiveEmail"));
+        // Dataprovider Kullanarak verilen email ve password bilgileri ile login olalım
+        registerPage.MailAddressTextBox.sendKeys(email);
 
+        registerPage.PasswordTextBox.sendKeys(password);
 
-        ExtentReportsListener.extentTestInfo("Password bölümüne geçerli bir şifre girilir");
-        registerPage.PasswordTextBox.sendKeys(ConfigReader.getProperty("registerPositivePassword"));
-
-
-        ExtentReportsListener.extentTestInfo("I agree to the privacy policy' checkbox ı işaretlenir");
         registerPage.AgreeCheckBox.click();
 
-        ExtentReportsListener.extentTestInfo("SingUP butonuna tıklanır");
         registerPage.SignUpButton.click();
 
-        ExtentReportsListener.extentTestInfo("'An account is already' uyrasının göründüğünü doğrulanır");
-        Assert.assertTrue(registerPage.RegistrationCheck.getText().contains("An account is already"));
-
-
-        /*@Test(description = "Siteye daha önce kayıtlı bilgiler ile(username ve email) kayıt yapılamamalı.(SignIn)")
-
-    public void testName() {
-
-        HomePage homePage= new HomePage();
-        SignInPage signInPage = new SignInPage();
-        ExtentReportsListener extentReportsListener=new ExtentReportsListener();
-
-        ExtentReportsListener.extentTestInfo("sayfaya gidilir");
-        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
-
-        ExtentReportsListener.extentTestInfo("SignIn butonuna tıklanır");
-        homePage.signIn.click();
-
-        ExtentReportsListener.extentTestInfo("Username bölümüne önceden kayıt olunan bir userName girilir");
-        signInPage.UsernameTextBox.sendKeys(ConfigReader.getProperty("signInUserName"));
-
-//        ExtentReportsListener.extentTestInfo("Email bölümüne kayıtlı bir email girilir");
-//        signInPage.MailAddressTextBox.sendKeys(ConfigReader.getProperty("signInEmail"));
-
-
-        ExtentReportsListener.extentTestInfo("Password bölümüne geçerli bir şifre girilir");
-        signInPage.PasswordTextBox.sendKeys(ConfigReader.getProperty("signInPassword"));
-
-
-        ExtentReportsListener.extentTestInfo("Remember me' checkbox ı işaretlenir");
-        signInPage.RemembeMeCheckBox.click();
-
-        ExtentReportsListener.extentTestInfo("SingIN butonuna tıklanır");
-        signInPage.SignInButton.click();
-
-//        ExtentReportsListener.extentTestInfo("'Login ' uyrasının göründüğünü doğrulanır");
-//        Assert.assertTrue(signInPage.RegistrationCheck.getText().contains("An account is already"));
+        ReusableMethods.visibleWait(registerPage.SignUpButton, 10);
+        Assert.assertTrue(registerPage.SignUpButton.isDisplayed());
 
 
 
-    }
-}*/
-    }
-}
+    }}
+
+
+
+
+
