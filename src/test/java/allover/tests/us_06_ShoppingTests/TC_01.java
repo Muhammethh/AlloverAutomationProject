1aceb49ca9e61c4d5d4bc32e3ac3319f9c4a4ba7
Muhammet Ölünk <molunk@gmail.com>
2025-02-15 15:44:50 +0300

    US19 bitti

--- src/test/java/allover/tests/us_06_ShoppingTests/TC_01.java
+++ src/test/java/allover/tests/us_06_ShoppingTests/TC_01.java
@@ -19,45 +19,60 @@ public class TC_01 {
         SampleItemsPage sampleItemsPage = new SampleItemsPage();
         CartPage cartPage = new CartPage();
         CheckOutPage checkOutPage = new CheckOutPage();
+        SignInPage signIn=new SignInPage();
 
         //SignIn Classından signIn metodu çağrılarak giriş yapılır
-        SignInCustomer.SignIn();
+        ExtentReportsListener.extentTestInfo("SignIn Classından signIn metodu çağrılarak giriş yapılır");
+        //SignInCustomer.SignIn();
+
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+        homePage.signIn.click();
+        signIn.UsernameTextBox.sendKeys(ConfigReader.getProperty("signInUserName"));
+        signIn.PasswordTextBox.sendKeys(ConfigReader.getProperty("signInPassword"));
+        signIn.SignInButton.click();
 
         //Sign Out butonu görünene kadar beklenir ve göründüğü doğrulanır
         WaitUtils.waitForVisibility(homePage.signOut, 10);
 
+        ExtentReportsListener.extentTestInfo("Sign Out butonu görünene kadar beklenir ve göründüğü doğrulanır");
         Assert.assertTrue(homePage.signOut.isDisplayed());
 
         //Search Bara geçerli bir ürün ismi girilir
+        ExtentReportsListener.extentTestInfo("Search Bara geçerli bir ürün ismi girilir ve aratılır");
         homePage.searchBox.sendKeys("Kazak", Keys.ENTER);
 
         //Çıkan ilk ürünün üzerine gelerek sepete ekle butonuna basılır
+        ExtentReportsListener.extentTestInfo("Çıkan ilk ürünün üzerine gelerek sepete ekle butonuna basılır");
         ActionsUtils.hoverOver(sampleItemsPage.firstItemAfterSearch);
-
-
         sampleItemsPage.addFirstItemInCart.click();
+
 
         ActionsUtils.hoverOver(homePage.cartHead);
         //Sepet simgesine basılarak küçük sepet penceresi açılır
         homePage.cartHead.click();
 
+
         //Sepet sayfasına gidilir
+        ExtentReportsListener.extentTestInfo("Sepet sayfasına gidilir");
         cartPage.ViewCartButton.click();
 
         //Proceed to Checkout butonuna basılır
+        ExtentReportsListener.extentTestInfo("Proceed to Checkout butonuna basılır");
         cartPage.proceedToCheckoutButton.click();
 
         ReusableMethods.visibleWait(checkOutPage.placeOrderButton,5);
         //Place Order butonuna basılır
+        ExtentReportsListener.extentTestInfo("Place Order butonuna basılır");
         ReusableMethods.scroll(checkOutPage.placeOrderButton);
 
         ReusableMethods.click(checkOutPage.placeOrderButton);
 
 
         //Siparişin başarılı olduğu doğrulanır
+        ExtentReportsListener.extentTestInfo("Siparişin başarılı olduğu doğrulanır");
         Assert.assertTrue(checkOutPage.orderCompletedText.isDisplayed());
 
-        Driver.closeDriver();
+        //Driver.closeDriver();
 
     }
 }
