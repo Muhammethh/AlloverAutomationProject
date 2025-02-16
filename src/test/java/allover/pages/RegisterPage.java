35eab180e9fd252e3cc46851b66fd3dea7d1cac2
Muhammet Çakmak <muhammetckmk46@gmail.com>
2025-02-17 00:46:01 +0300

    MuhammetC

--- src/test/java/allover/pages/RegisterPage.java
+++ src/test/java/allover/pages/RegisterPage.java
@@ -23,18 +23,28 @@ public class RegisterPage {
     @FindBy(css = "input[id='register-policy']")
     public WebElement AgreeCheckBox;
 
-   @FindBy(css = "input[id='register-policy']")
-   public WebElement AgreeCheckBox;
-
-   @FindBy(xpath = "(//button[@value='Sign Up'])[1]")
-   public WebElement SignUpButton;
+    @FindBy(xpath = "(//button[@value='Sign Up'])[1]")
+    public WebElement SignUpButton;
 
     @FindBy(partialLinkText = "Signup as a vendor?")
-   public WebElement VendorSignUpButton;
+    public WebElement VendorSignUpButton;
 
     @FindBy(partialLinkText = "Become a Vendor")
-   public WebElement BecomeVendorButton;
+    public WebElement BecomeVendorButton;
 
-    @FindBy(xpath = "(//p[@class='submit-status'])[2]")
+     @FindBy(xpath = "(//p[@class='submit-status'])[2]")
     public WebElement RegistrationCheck;
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/allover/tests/US_02_Register_Negative/TC01.java
+++ src/test/java/allover/tests/US_02_Register_Negative/TC01.java
@@ -1,24 +1,21 @@
-package allover.tests.US_02;
+package allover.tests.US_02_Register_Negative;
 
-import allover.pages.AccountDetailsPage;
 import allover.pages.HomePage;
 import allover.pages.RegisterPage;
-import allover.pages.SignInPage;
 import allover.utilities.ConfigReader;
 import allover.utilities.Driver;
 import allover.utilities.ExtentReportsListener;
-import allover.utilities.WaitUtils;
 import org.testng.Assert;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class TC01 {
 
     @Test(description = "Siteye daha önce kayıtlı bilgiler ile(username ve email) kayıt yapılamamalı.(Register)")
     public void testName() {
-        HomePage homePage= new HomePage();
-        RegisterPage registerPage= new RegisterPage();
-        ExtentReportsListener extentReportsListener=new ExtentReportsListener();
+
+        HomePage homePage = new HomePage();
+        RegisterPage registerPage = new RegisterPage();
+        ExtentReportsListener extentReportsListener = new ExtentReportsListener();
 
         ExtentReportsListener.extentTestInfo("sayfaya gidilir");
         Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
@@ -26,16 +23,21 @@ public class TC01 {
         ExtentReportsListener.extentTestInfo("register butonuna tıklanır");
         homePage.register.click();
 
+<<<<<<< HEAD:src/test/java/allover/tests/US_02_Register_Negative/TC01.java
 
         ExtentReportsListener.extentTestInfo("Username bölümüne önceden kayıt olunan bir userName girilir");
+        registerPage.UsernameTextBox.sendKeys(ConfigReader.getProperty("registerUsedUsername"));
+=======
+        ExtentReportsListener.extentTestInfo("Username bölümüne önceden kayıt olunan bir username girilir");
         registerPage.UsernameTextBox.sendKeys(ConfigReader.getProperty("registerPositiveUsername"));
+>>>>>>> master:src/test/java/allover/tests/US_02/TC01.java
 
         ExtentReportsListener.extentTestInfo("Email bölümüne kayıtlı bir email girilir");
-        registerPage.MailAddressTextBox.sendKeys(ConfigReader.getProperty("registerPositiveEmail"));
+        registerPage.MailAddressTextBox.sendKeys(ConfigReader.getProperty("registerUsedEmail"));
 
 
         ExtentReportsListener.extentTestInfo("Password bölümüne geçerli bir şifre girilir");
-        registerPage.PasswordTextBox.sendKeys(ConfigReader.getProperty("registerPositivePassword"));
+        registerPage.PasswordTextBox.sendKeys(ConfigReader.getProperty("registerUsedPassword"));
 
 
         ExtentReportsListener.extentTestInfo("I agree to the privacy policy' checkbox ı işaretlenir");
@@ -47,8 +49,11 @@ public class TC01 {
         ExtentReportsListener.extentTestInfo("'An account is already' uyrasının göründüğünü doğrulanır");
         Assert.assertTrue(registerPage.RegistrationCheck.getText().contains("An account is already"));
 
+<<<<<<< HEAD:src/test/java/allover/tests/US_02_Register_Negative/TC01.java
         //sayfa kapatılır
         Driver.closeDriver();
 
+=======
+>>>>>>> master:src/test/java/allover/tests/US_02/TC01.java
     }
 }
