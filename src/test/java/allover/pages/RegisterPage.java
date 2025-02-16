0d4c76f8041efc70969031aef95f5d1d12d6a464
cerit123 <hafizeozcerit@gmail.com>
2025-02-16 00:17:38 +0000

    Extend

--- src/test/java/allover/pages/RegisterPage.java
+++ src/test/java/allover/pages/RegisterPage.java
@@ -23,18 +23,20 @@ public class RegisterPage {
     @FindBy(css = "input[id='register-policy']")
     public WebElement AgreeCheckBox;
 
-   @FindBy(css = "input[id='register-policy']")
-   public WebElement AgreeCheckBox;
+    @FindBy(xpath = "(//button[@value='Sign Up'])[1]")
 
-   @FindBy(xpath = "(//button[@value='Sign Up'])[1]")
    public WebElement SignUpButton;
 
+    public WebElement SignUpButton;
+
+
     @FindBy(partialLinkText = "Signup as a vendor?")
-   public WebElement VendorSignUpButton;
+    public WebElement VendorSignUpButton;
 
     @FindBy(partialLinkText = "Become a Vendor")
-   public WebElement BecomeVendorButton;
+    public WebElement BecomeVendorButton;
 
     @FindBy(xpath = "(//p[@class='submit-status'])[2]")
     public WebElement RegistrationCheck;
+
 }
