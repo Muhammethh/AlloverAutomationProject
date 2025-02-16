7e0cff551a2e1cede9aae6d9368bc7aafb00e8db
Fatma Yasar <fatmasemizyasar2023@gmail.com>
2025-02-16 16:11:26 +0100

    takescreenshot eklendi

--- src/test/java/allover/tests/us_12_VendorBillingAddresses/TC_05.java
+++ src/test/java/allover/tests/us_12_VendorBillingAddresses/TC_05.java
@@ -75,6 +75,7 @@ public class TC_05 extends SignInVendor {
 //          ZIP Code kısmına geçersiz veri girilir
         vendorAdressesPage.zipCode.clear();
         vendorAdressesPage.zipCode.sendKeys("a123?");
+        ExtentReportsListener.addScreenShotToReport();
         ExtentReportsListener.extentTestInfo(" ZIP Code kısmına geçersiz veri girilir");
 
 
@@ -96,6 +97,7 @@ public class TC_05 extends SignInVendor {
 
 //      "Address changed successfully." metni görülmedigi doğrulanır
         assertTrue(vendorAdressesPage.zipcodeverfy.isDisplayed());
+        ExtentReportsListener.addScreenShotToReport();
         ExtentReportsListener.extentTestFail("ZipCode alanı hatalı girildiğinde adres eklenmemeli ancak eklendi!");
 
         Driver.closeDriver();
