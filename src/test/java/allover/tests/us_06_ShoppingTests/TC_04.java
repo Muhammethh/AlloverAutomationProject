2dd7f4e51bafc544c19bf3c7361ec4550755be52
Muhammet Ölünk <molunk@gmail.com>
2025-02-18 13:10:32 +0300

    son düzenleme

--- src/test/java/allover/tests/us_06_ShoppingTests/TC_04.java
+++ src/test/java/allover/tests/us_06_ShoppingTests/TC_04.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class TC_04 extends Notations{
+public class TC_04{
 
     @Test
     public void TC_04_IsBillingDetailVisible() {
@@ -18,13 +18,13 @@ public class TC_04 extends Notations{
         CheckOutPage checkOutPage = new CheckOutPage();
         SoftAssert softAssert = new SoftAssert();
 
-        //SignInCustomer.SignIn();
-       // SignInPage signIn=new SignInPage();
-       // Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
-       // homePage.signIn.click();
-       // signIn.UsernameTextBox.sendKeys(ConfigReader.getProperty("signInUserName"));
-       // signIn.PasswordTextBox.sendKeys(ConfigReader.getProperty("signInPassword"));
-       // signIn.SignInButton.click();
+
+        SignInPage signIn=new SignInPage();
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+        homePage.signIn.click();
+        signIn.UsernameTextBox.sendKeys(ConfigReader.getProperty("signInUserName"));
+        signIn.PasswordTextBox.sendKeys(ConfigReader.getProperty("signInPassword"));
+        signIn.SignInButton.click();
 
         //Sign Out butonu görünene kadar beklenir ve göründüğü doğrulanır
         WaitUtils.waitForVisibility(homePage.signOut, 10);
@@ -80,7 +80,7 @@ public class TC_04 extends Notations{
 
         softAssert.assertAll();
 
-        //Driver.closeDriver();
+        Driver.closeDriver();
 
     }
 }
